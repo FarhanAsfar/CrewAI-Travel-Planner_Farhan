0557756d596a37b385fb9ca285600ee6ac01bdde
farhan <farhanasfar21@gmail.com>
2026-02-26 15:07:29 +0600

    Remove comments

--- src/travel_planner/main.py
+++ src/travel_planner/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import sys
 import warnings
 
@@ -7,11 +6,6 @@ from datetime import datetime
 from travel_planner.crew import TravelPlanner
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
-
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
 
 def run():
     """
